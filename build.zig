79ef9949f080cf533ec8a4a21b078d07d8149e4b
roy.crippen4 <roy.crippen4@archarithms.com>
2026-02-21 20:02:07 -0500

    fix: consumer build error
    missing -Doptimize flag error when building in a consumer project

--- build.zig
+++ build.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("aoc", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     const test_filters = b.option(
